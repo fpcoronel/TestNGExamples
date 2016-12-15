9eee974e7791f9a0eaf081fc39e3013003f149a3
fpcoronel <fcoronel@hxnb-fcoronel.hexacta.com>
2016-12-15 20:26:27 -0300

    Moving to POM - Partial job

--- src/com/howtotestngdo/test/FirstTest.java
+++ src/com/howtotestngdo/test/FirstTest.java
@@ -30,51 +30,6 @@ public class FirstTest extends DriverSetUp {
 		driver = getDriver();
 	}
 	
-	/*public void initializeTestBaseSetup(String browserType, String appURL) {
-		try {
-			setDriver(browserType, appURL);
-
-		} catch (Exception e) {
-			System.out.println("Error....." + e.getStackTrace());
-		}
-	}*/
-
-	/*private void setDriver(String browserType, String appURL) {
-		switch (browserType) {
-		case "chrome":
-			driver = initChromeDriver(appURL);
-			break;*/
-		/*
-		 * case "firefox": driver = initFirefoxDriver(appURL); break;
-		 */
-		/*default:
-			System.out.println("browser : " + browserType + " is invalid");// ,
-																			// Launching
-																			// Firefox
-																			// as
-																			// browser
-																			// of
-																			// choice..");
-			// driver = initFirefoxDriver(appURL);
-		}
-	}*/
-
-	/*private static WebDriver initChromeDriver(String appURL) {
-		System.out.println("Launching google chrome with new profile..");
-		System.setProperty("webdriver.chrome.driver", "C:/Selenium/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.navigate().to(appURL);
-		return driver;
-	}*/
-
-	/*
-	 * private static WebDriver initFirefoxDriver(String appURL) {
-	 * System.out.println("Launching Firefox browser.."); WebDriver driver = new
-	 * FirefoxDriver(); driver.manage().window().maximize();
-	 * driver.navigate().to(appURL); return driver; }
-	 */
-	
 	
 	@Test(description = "Lamps Plus site - Main page - Title")
 	public void launchSite() {
